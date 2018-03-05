0e92004eb03c5299f518da79fad2ea3eab81802e
Sava <sd.sakhro@gmail.com>
2018-03-05 21:00:20 +0200

    Add and ajust gulp-sass and gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', defaultTask);
+gulp.task('default', () => {
+    gulp.watch('css/sass/*.scss', ['styles'])
+});
 
-function defaultTask(done) {
-    // place code for your default task here
-    done();
-}
+gulp.task('styles', () => {
+    gulp.src('css/sass/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('./css'));
+});
